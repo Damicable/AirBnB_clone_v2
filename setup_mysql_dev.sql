cbf4fa8dde6636e178baa09812f2742857797c48
Ay-slim <ayooluwaadedipe@gmail.com>
2023-09-19 03:13:34 -0700

    Add space after comments prefix

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,4 +1,4 @@
---Prepares a Mysql server for the AirBnB project
+-- Prepares a Mysql server for the AirBnB project
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 GRANT ALL ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
