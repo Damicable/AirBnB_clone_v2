38f22ed65f6eef5f5a66e2def134b3c987bbac14
Damicable <annobidiegwu@gmail.com>
2023-10-05 20:52:37 -0700

    Compress before sending

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 # Fabric script to generates a .tgz archive from the contents of web_static
 
+import os
 from fabric import task
 from datetime import datetime
-import os
 
 
 @task
