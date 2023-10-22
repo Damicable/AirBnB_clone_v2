76361b6949b55a80cb5e9b4acdd2f11fda4914f2
Damicable <annobidiegwu@gmail.com>
2023-10-22 15:19:33 -0700

    Updated

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,23 +1,17 @@
 #!/usr/bin/python3
-"""
-Simple Flask web application script that displays a list of states.
-"""
+"""Simple Flask web application script that displays list of states."""
 
 from flask import Flask, render_template
 from models import storage
 from models.state import State
 
-app = Flask(__name)
+app = Flask(__name__)
 
 
-@app.route('/states_list', strict_slashes = False)
-def list_state():
+@app.route('/states_list', strict_slashes=False)
+def states_list():
     """This is /states_list route that a list of states."""
-    states_list = []
-    all_states = storage.all(State)
-    for st in all_states:
-        states_list.append(all_states[st])
-    states = sorted(states_list, key=lambda x: x.name)
+    states = storage.all(State).values()
     return render_template('7-states_list.html', states=states)
 
 
