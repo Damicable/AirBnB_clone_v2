2b273d3c21aefd8642da7b6c1881df438448b4bc
Damicable <annobidiegwu@gmail.com>
2023-10-25 19:34:58 -0700

    Cities by state

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,40 +1,40 @@
 #!/usr/bin/python3
-"""
-Flask web application script that displays a list of states and cities.
-"""
+"""Flask web application to display cities by state"""
 
 from flask import Flask, render_template
 from models import storage
-from models.state import State
 from models.city import City
+from models.state import State
 
 app = Flask(__name__)
 
 
 @app.route("/cities_by_states", strict_slashes=False)
-def fetch_states():
-    """Fetch states function definition"""
+def display_cities_by_states():
+    """Display a list of states and their cities"""
+    states = storage.all(State)
     states_list = []
-    all_states = storage.all(State)
-    all_cities = storage.all(City)
-    for st in all_states:
-        a_s_d = {}
-        ct_list = []
-        for ct in all_cities:
-            if all_cities[ct].state_id == all_states[st].id:
-                tmp = {}
-                tmp['name'] = all_cities[ct].name
-                tmp['id'] = all_cities[ct].id
-                ct_list.append(tmp)
-        a_s_d = all_states[st].__dict__
-        a_s_d['name'] = all_states[st].name
-        a_s_d['id'] = all_states[st].id
-        sorted_cts = sorted(ct_list, key=lambda x: x['name'])
-        a_s_d['cts'] = sorted_cts
-        states_list.append(a_s_d)
-    states = sorted(states_list, key=lambda x: x['name'])
-    return render_template('8-cities_by_states.html', states=states)
+    for state_id, state in states.items():
+        state_dict = {
+            "id": state.id,
+            "name": state.name,
+            "cities": sorted(
+                [
+                    {"id": city.id, "name": city.name}
+                    for city in state.cities
+                ],
+                key=lambda x: x["name"]
+            )
+        }
+        states_list.append(state_dict)
+    
+    states_list = sorted(states_list, key=lambda x: x["name"])
+    return render_template('8-cities_by_states.html', states_list=states_list)
 
+@app.teardown_appcontext
+def teardown_session(exception):
+    """Closes the current SQLAlchemy Session after each request."""
+    storage.close()
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host="0.0.0.0", port=5000)
