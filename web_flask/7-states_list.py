65f527f8d08e3b2d9561cfc6d81ca925de6699f4
Damicable <annobidiegwu@gmail.com>
2023-11-14 00:30:20 -0800

    UPdated

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,5 +1,5 @@
-!/usr/bin/python3
-"""This is a simple Flask web application script that displays all states."""
+#!/usr/bin/python3
+"""Module containing list_state route"""
 
 from flask import Flask, render_template
 from models import storage
@@ -8,19 +8,21 @@ from models.state import State
 app = Flask(__name__)
 
 
-@app.route('/states_list', strict_slashes=False)
-def display_states():
-    """This route fetches and lists all states."""
-    states = storage.all(State)
-    states = sorted(states.values(), key=lambda x: x.name)
-    return render_template('7-states_list.html', all_state=states)
+@app.route("/states_list", strict_slashes=False)
+def fetch_states():
+    """Fetch states function definition"""
+    states_list = []
+    all_states = storage.all(State)
+    for st in all_states:
+        states_list.append(all_states[st])
+    a_s = sorted(states_list, key=lambda x: x.name)
+    return render_template('7-states_list.html', a_s=a_s)
 
 
 @app.teardown_appcontext
-def teardown_session(exception):
-    """Closes the current SQLAlchemy Session after each request."""
+def app_teardown(exception):
     storage.close()
 
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000)
+    app.run()
