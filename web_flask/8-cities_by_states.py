81c5a5c61ae4cb2e12e08eea621dbc8e4793d006
Damicable <annobidiegwu@gmail.com>
2023-11-14 00:54:55 -0800

    updated

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,35 +1,37 @@
 #!/usr/bin/python3
-"""Flask web application to display cities by state"""
+"""Module containing cities by state route"""
 
 from flask import Flask, render_template
 from models import storage
-from models.city import City
 from models.state import State
+from models.city import City
 
 app = Flask(__name__)
 
 
 @app.route("/cities_by_states", strict_slashes=False)
-def display_cities_by_states():
-    """Display a list of states and their cities"""
-    states = storage.all(State)
+def fetch_states():
+    """Fetch states function definition"""
     states_list = []
-    for state_id, state in states.items():
-        state_dict = {
-            "id": state.id,
-            "name": state.name,
-            "cities": sorted(
-                [
-                    {"id": city.id, "name": city.name}
-                    for city in state.cities
-                ],
-                key=lambda x: x["name"]
-            )
-        }
-        states_list.append(state_dict)
-
-    state_list = sorted(states_list, key=lambda x: x["name"])
-    return render_template('8-cities_by_states.html', state_list=state_list)
+    all_states = storage.all(State)
+    all_cities = storage.all(City)
+    for st in all_states:
+        a_s_d = {}
+        ct_list = []
+        for ct in all_cities:
+            if all_cities[ct].state_id == all_states[st].id:
+                tmp = {}
+                tmp['name'] = all_cities[ct].name
+                tmp['id'] = all_cities[ct].id
+                ct_list.append(tmp)
+        a_s_d = all_states[st].__dict__
+        a_s_d['name'] = all_states[st].name
+        a_s_d['id'] = all_states[st].id
+        sorted_cts = sorted(ct_list, key=lambda x: x['name'])
+        a_s_d['cts'] = sorted_cts
+        states_list.append(a_s_d)
+    a_s = sorted(states_list, key=lambda x: x['name'])
+    return render_template('8-cities_by_states.html', a_s=a_s)
 
 
 @app.teardown_appcontext
